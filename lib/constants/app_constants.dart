d8ec284f6dd45466d24994e5ff0cf1110e0deee1
Shaijo George <shaijo.george@cofsosupport.in>
2025-06-22 00:07:56 +0530

    Created app_constants.dart with color, size, and string constants

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  //Colors
+  static const Color backgroundColor = Colors.black;
+  static const Color textColor = Colors.white;
+  
+  //Sizes
+
+
+  //Strings
+  static const String appTitle = 'Numio';
+}
